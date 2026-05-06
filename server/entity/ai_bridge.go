35a54fc66d9307e754c474d576af2bcbc88a79e4
AssassinGhostYT <assassinghostyt@gmail.com>
2026-05-06 03:23:26 +0000

    feat: implement Silverfish entity using MobsX-MC AI library

---
+++ server/entity/ai_bridge.go
@@ -0,0 +1,71 @@
+package entity
+
+import (
+	"github.com/AssassinGhostYT/MobsX-MC/api"
+	"github.com/AssassinGhostYT/MobsX-MC/mmath"
+	"github.com/df-mc/dragonfly/server/block/cube"
+	"github.com/df-mc/dragonfly/server/world"
+)
+
+// worldBridge implements MobsX-MC api.World.
+type worldBridge struct {
+	tx *world.Tx
+}
+
+func (w worldBridge) Block(pos mmath.Pos) api.Block {
+	b := w.tx.Block(cube.Pos{int(pos.X), int(pos.Y), int(pos.Z)})
+	return blockBridge{b: b}
+}
+
+func (w worldBridge) Entities() []api.Entity {
+	return nil
+}
+
+// blockBridge implements MobsX-MC api.Block.
+type blockBridge struct {
+	b world.Block
+}
+
+func (b blockBridge) Name() string {
+	if n, ok := b.b.(interface{ EncodeBlock() (string, map[string]any) }); ok {
+		name, _ := n.EncodeBlock()
+		return name
+	}
+	return "minecraft:air"
+}
+
+func (b blockBridge) Solid() bool {
+	return b.Name() != "minecraft:air" 
+}
+
+func (b blockBridge) Passable() bool {
+	return b.Name() == "minecraft:air"
+}
+
+// EntityBridge implements MobsX-MC api.Entity.
+type EntityBridge struct {
+	E  *Ent
+	Tx *world.Tx
+}
+
+func (e EntityBridge) Position() [3]float64 {
+	pos := e.E.Position()
+	return [3]float64{pos.X(), pos.Y(), pos.Z()}
+}
+
+func (e EntityBridge) SetPosition(pos [3]float64) {}
+
+func (e EntityBridge) Rotation() [2]float32 {
+	rot := e.E.Rotation()
+	return [2]float32{float32(rot.Yaw()), float32(rot.Pitch())}
+}
+
+func (e EntityBridge) SetRotation(yaw, pitch float32) {}
+
+func (e EntityBridge) ID() int64 {
+	return int64(e.E.H().RuntimeID())
+}
+
+func (e EntityBridge) HideInBlock(pos mmath.Pos) {}
+
+func (e EntityBridge) AlertOthers(rangeX, rangeY, rangeZ int) {}
---
+++ server/entity/silverfish.go
@@ -0,0 +1,103 @@
+package entity
+
+import (
+	"github.com/AssassinGhostYT/MobsX-MC"
+	"github.com/AssassinGhostYT/MobsX-MC/behavior"
+	"github.com/AssassinGhostYT/MobsX-MC/sensor"
+	"github.com/df-mc/dragonfly/server/block/cube"
+	"github.com/df-mc/dragonfly/server/world"
+	"github.com/df-mc/dragonfly/server/world/sound"
+	"github.com/go-gl/mathgl/mgl64"
+	"math/rand"
+)
+
+// Silverfish is a small insect-like hostile mob.
+type Silverfish struct {
+	brain     *mobsx.Brain
+	navigator *mobsx.Navigator
+	mc        *MovementComputer
+
+	health    float64
+	alerted   *behavior.CallForHelpBehavior
+}
+
+// NewSilverfish creates a new Silverfish entity handle.
+func NewSilverfish(opts world.EntitySpawnOpts) *world.EntityHandle {
+	return opts.New(SilverfishType, Silverfish{health: 8})
+}
+
+// Apply ...
+func (s Silverfish) Apply(data *world.EntityData) {
+	s.mc = &MovementComputer{Gravity: 0.08, Drag: 0.02}
+	data.Data = &s
+}
+
+// Tick ...
+func (s *Silverfish) Tick(e *Ent, tx *world.Tx) *Movement {
+	if s.brain == nil {
+		s.brain = mobsx.NewBrain()
+		wBridge := worldBridge{tx: tx}
+		s.navigator = mobsx.NewNavigator(EntityBridge{E: e, Tx: tx}, wBridge)
+		s.navigator.Speed = 0.2 
+
+		playerScanner := &sensor.PlayerSensor{Range: 16}
+		s.alerted = &behavior.CallForHelpBehavior{RangeX: 21, RangeY: 11, RangeZ: 21}
+		
+		s.brain.AddSensor(playerScanner)
+		s.brain.AddBehavior(s.alerted)
+		s.brain.AddBehavior(behavior.NewAttack(playerScanner, s.navigator))
+		s.brain.AddBehavior(&behavior.InfestBehavior{InfestChance: 0.01})
+		s.brain.AddBehavior(behavior.NewWander(s.navigator, 10))
+	}
+	
+	s.brain.Tick(EntityBridge{E: e, Tx: tx}, worldBridge{tx: tx})
+	
+	if rand.Intn(100) == 0 {
+		tx.PlaySound(e.Position(), sound.SilverfishAmbient{})
+	}
+	
+	m := s.mc.TickMovement(e, e.data.Pos, e.data.Vel, e.data.Rot, tx)
+	e.data.Pos, e.data.Vel = m.pos, m.vel
+	return m
+}
+
+// SilverfishType ...
+var SilverfishType silverfishType
+
+type silverfishType struct{}
+
+func (t silverfishType) Open(tx *world.Tx, handle *world.EntityHandle, data *world.EntityData) world.Entity {
+	return &Ent{tx: tx, handle: handle, data: data}
+}
+func (silverfishType) EncodeEntity() string { return "minecraft:silverfish" }
+func (silverfishType) BBox(world.Entity) cube.BBox {
+	return cube.Box(-0.15, 0, -0.15, 0.15, 0.3, 0.15)
+}
+func (silverfishType) DecodeNBT(_ map[string]any, data *world.EntityData) {
+	Silverfish{health: 8}.Apply(data)
+}
+func (silverfishType) EncodeNBT(*world.EntityData) map[string]any { return nil }
+
+// Living methods
+func (s *Silverfish) Health() float64 { return s.health }
+func (s *Silverfish) MaxHealth() float64 { return 8 }
+func (s *Silverfish) SetMaxHealth(v float64) { s.health = v }
+func (s *Silverfish) Dead() bool { return s.health <= 0 }
+func (s *Silverfish) Hurt(damage float64, src world.DamageSource) (n float64, v bool) {
+	s.health -= damage
+	if s.alerted != nil {
+		s.alerted.Alerted = true
+	}
+	return damage, true
+}
+func (s *Silverfish) Heal(health float64, src world.HealingSource) { s.health += health }
+func (s *Silverfish) KnockBack(src mgl64.Vec3, f, h float64) {}
+func (s *Silverfish) Velocity() mgl64.Vec3 { return mgl64.Vec3{} }
+func (s *Silverfish) SetVelocity(v mgl64.Vec3) {}
+func (s *Silverfish) Speed() float64 { return 0.2 }
+func (s *Silverfish) SetSpeed(v float64) {}
+func (s *Silverfish) AddEffect(e any) {}
+func (s *Silverfish) RemoveEffect(e any) {}
+func (s *Silverfish) Effects() []any { return nil }
+func (s *Silverfish) PistonImmovable() bool { return false }
+func (s *Silverfish) PistonBreakable() bool { return false }
